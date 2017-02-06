686342e50a026ffb1bd7de9ddaa8c1b70bf47449
Rellika Kisyula <kisyular@msu.edu>
2017-02-06 00:10:22 -0500

    vCannot read property 'value' of null at HTMLTextAreaElement.myFunction

--- js/script.js
+++ js/script.js
@@ -1,53 +1,53 @@
+// I am getting errors :Uncaught TypeError: Cannot read property 'value' of null at HTMLTextAreaElement.myFunction
+document.getElementById('bio').addEventListener("keyup", myFunction);
+
+function myFunction(inID, outID) {
+  var x = document.getElementById(inID).value;
+  document.getElementById(outID).innerHTML = x
+}
+myFunction('bio', 'describeYourself')
+
+//document.getElementById('bio').addEventListener("keyup", myFunction);
 /*
+    /*
+    function firstName() {
+      var targetTag = document.getElementById('first')
+      var x = targetTag.value
+      document.getElementById('spanFirst').innerHTML = x[0].toUpperCase() + x.slice(1)
+      var textInside = document.getElementById('spanFirst').textContent
+      document.getElementById('preCode').textContent = '<h4>' + textInside + '</h4>'
+    }
 
-I am getting errors here so that is why i decided not to use eventlistener
-document.getElementById("bio").addEventListener("keyup", myFunction);
+    function lastName() {
+      var targetTag = document.getElementById('last')
+      var x = targetTag.value
+      document.getElementById('spanLast').innerHTML = x[0].toUpperCase() + x.slice(1)
+      var textInside = document.getElementById('spanLast').textContent
+      document.getElementById('preCode').textContent = '<h4>' + textInside + '</h4>'
+    }
 
-function lastName(inID, outID) {
-  var targetTag = document.getElementById(inID)
-  var x = targetTag.value
-  document.getElementById(outID).innerHTML = x[0].toUpperCase() + x.slice(1)
-}
-lastName('last', "spanLast")
-*/
-//var global = ''
-function firstName() {
-  var targetTag = document.getElementById('first')
-  var x = targetTag.value
-  document.getElementById('spanFirst').innerHTML = x[0].toUpperCase() + x.slice(1)
-  var textInside = document.getElementById('spanFirst').textContent
-  document.getElementById('preCode').textContent = '<h4>' + textInside + '</h4>'
-}
+    function bio() {
+      var targetTag = document.getElementById('bio')
+      var x = targetTag.value
+      document.getElementById('describeYourself').innerHTML = x[0].toUpperCase() + x.slice(1)
+      var textInside = document.getElementById('describeYourself').textContent
+      //global = global + test
+      document.getElementById('preCode').textContent = '<p>' + textInside + '</p>'
+    }
 
-function lastName() {
-  var targetTag = document.getElementById('last')
-  var x = targetTag.value
-  document.getElementById('spanLast').innerHTML = x[0].toUpperCase() + x.slice(1)
-  var textInside = document.getElementById('spanLast').textContent
-  document.getElementById('preCode').textContent = '<h4>' + textInside + '</h4>'
-}
+    function emailAddress() {
+      var targetTag = document.getElementById('email')
+      var x = targetTag.value
+      document.getElementById('emailOut').innerHTML = x
+      var textInside = document.getElementById('emailOut').textContent
+      document.getElementById('preCode').textContent = '<a>' + textInside + '</a>'
+    }
 
-function bio() {
-  var targetTag = document.getElementById('bio')
-  var x = targetTag.value
-  document.getElementById('describeYourself').innerHTML = x[0].toUpperCase() + x.slice(1)
-  var textInside = document.getElementById('describeYourself').textContent
-  //global = global + test
-  document.getElementById('preCode').textContent = '<p>' + textInside + '</p>'
-}
-
-function emailAddress() {
-  var targetTag = document.getElementById('email')
-  var x = targetTag.value
-  document.getElementById('emailOut').innerHTML = x
-  var textInside = document.getElementById('emailOut').textContent
-  document.getElementById('preCode').textContent = '<a>' + textInside + '</a>'
-}
-
-function phoneNumber() {
-  var targetTag = document.getElementById('phone')
-  var x = targetTag.value
-  document.getElementById('phoneOut').innerHTML = x
-  var textInside = document.getElementById('phoneOut').textContent
-  document.getElementById('preCode').textContent = '<a>' + textInside + '</a>'
-}
+    function phoneNumber() {
+      var targetTag = document.getElementById('phone')
+      var x = targetTag.value
+      document.getElementById('phoneOut').innerHTML = x
+      var textInside = document.getElementById('phoneOut').textContent
+      document.getElementById('preCode').textContent = '<a>' + textInside + '</a>'
+    }
+    */
