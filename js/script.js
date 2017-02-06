991141fedf668bd669227c917d60d0a0e14cc14d
Rellika Kisyula <kisyular@msu.edu>
2017-02-06 12:38:18 -0500

    fixed the error :Cannot read property 'value' of null at HTMLTextAreaElement.myFunction

--- js/script.js
+++ js/script.js
@@ -1,53 +1,33 @@
-// I am getting errors :Uncaught TypeError: Cannot read property 'value' of null at HTMLTextAreaElement.myFunction
-document.getElementById('bio').addEventListener("keyup", myFunction);
+document.getElementById('first').addEventListener('keyup', firstName)
 
-function myFunction(inID, outID) {
-  var x = document.getElementById(inID).value;
-  document.getElementById(outID).innerHTML = x
+function firstName() {
+  var x = document.getElementById('first').value
+  document.getElementById('spanFirst').innerHTML = x
 }
-myFunction('bio', 'describeYourself')
 
-//document.getElementById('bio').addEventListener("keyup", myFunction);
-/*
-    /*
-    function firstName() {
-      var targetTag = document.getElementById('first')
-      var x = targetTag.value
-      document.getElementById('spanFirst').innerHTML = x[0].toUpperCase() + x.slice(1)
-      var textInside = document.getElementById('spanFirst').textContent
-      document.getElementById('preCode').textContent = '<h4>' + textInside + '</h4>'
-    }
+document.getElementById('last').addEventListener('keyup', lastName)
 
-    function lastName() {
-      var targetTag = document.getElementById('last')
-      var x = targetTag.value
-      document.getElementById('spanLast').innerHTML = x[0].toUpperCase() + x.slice(1)
-      var textInside = document.getElementById('spanLast').textContent
-      document.getElementById('preCode').textContent = '<h4>' + textInside + '</h4>'
-    }
+function lastName() {
+  var x = document.getElementById('last').value
+  document.getElementById('spanLast').innerHTML = x
+}
 
-    function bio() {
-      var targetTag = document.getElementById('bio')
-      var x = targetTag.value
-      document.getElementById('describeYourself').innerHTML = x[0].toUpperCase() + x.slice(1)
-      var textInside = document.getElementById('describeYourself').textContent
-      //global = global + test
-      document.getElementById('preCode').textContent = '<p>' + textInside + '</p>'
-    }
+document.getElementById('bio').addEventListener('keyup', bioGraph)
 
-    function emailAddress() {
-      var targetTag = document.getElementById('email')
-      var x = targetTag.value
-      document.getElementById('emailOut').innerHTML = x
-      var textInside = document.getElementById('emailOut').textContent
-      document.getElementById('preCode').textContent = '<a>' + textInside + '</a>'
-    }
+function bioGraph() {
+  var x = document.getElementById('bio').value
+  document.getElementById('describeYourself').innerHTML = x
+}
+document.getElementById('email').addEventListener('keyup', addressEmail)
 
-    function phoneNumber() {
-      var targetTag = document.getElementById('phone')
-      var x = targetTag.value
-      document.getElementById('phoneOut').innerHTML = x
-      var textInside = document.getElementById('phoneOut').textContent
-      document.getElementById('preCode').textContent = '<a>' + textInside + '</a>'
-    }
-    */
+function addressEmail() {
+  var x = document.getElementById('email').value
+  document.getElementById('emailOut').innerHTML = x
+}
+
+document.getElementById('phone').addEventListener('keyup', numberPhone)
+
+function numberPhone() {
+  var x = document.getElementById('phone').value
+  document.getElementById('phoneOut').innerHTML = x
+}
