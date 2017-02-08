40de1be2cb764519ec8f452125ae85a33d381058
Rellika Kisyula <kisyular@msu.edu>
2017-02-08 02:50:41 -0500

    Fixed major Project Functions. Shortened the Code

--- js/script.js
+++ js/script.js
@@ -1,41 +1,29 @@
 document.getElementById('first').addEventListener('keyup', firstName)
 
 function firstName() {
-  var x = document.getElementById('first').value
-  document.getElementById('spanFirst').innerHTML = x
   updateProfile()
 }
 
 document.getElementById('last').addEventListener('keyup', lastName)
 
 function lastName() {
-  var x = document.getElementById('last').value
-  document.getElementById('spanLast').innerHTML = x
   updateProfile()
 }
 
 document.getElementById('bio').addEventListener('keyup', bioGraph)
 
 function bioGraph() {
-  var x = document.getElementById('bio').value
-  document.getElementById('describeYourself').innerHTML = x
   updateProfile()
 }
 document.getElementById('email').addEventListener('keyup', addressEmail)
 
 function addressEmail() {
-  var x = document.getElementById('email').value
-  document.getElementById('emailOut').innerHTML = x
-  document.getElementById('emailOut').href = 'mailto:' + x;
   updateProfile()
 }
 
 document.getElementById('phone').addEventListener('keyup', numberPhone)
 
 function numberPhone() {
-  var x = document.getElementById('phone').value
-  document.getElementById('phoneOut').innerHTML = x
-  document.getElementById('phoneOut').href = 'tel:' + x;
   updateProfile()
 }
 
@@ -46,6 +34,9 @@ function updateProfile() {
   var email = document.getElementById('email').value
   var phone = document.getElementById('phone').value
 
-  var profile = firstName + ' ' + lastName + ' ' + bioGraph + ' ' + email + ' ' + phone
+  var profile = '<h4>Hi, my name is ' + firstName + ' ' + lastName + '</h4>' + '<p>' + ' ' + bioGraph + '</p>' + ' ' + '<p> If you\'re interested in a date, you can email me at' + ' ' + '<a id="emailOut" target="_blank">' + email + '</a>' + ' ' + 'or give me a call at' + ' ' + '<a id="phoneOut" target="_blank">' + phone + '</a>' + '</p>'
   document.getElementById('preCode').textContent = profile
+  document.getElementById('preview').innerHTML = profile
+  document.getElementById('emailOut').href = 'mailto:' + email;
+  document.getElementById('phoneOut').href = 'tel:' + phone;
 }
